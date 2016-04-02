83dfa397df1130dbc899a828ca2d474c89c599ad
Steffen Gutzeit <steffen.gutzeit@web.de>
2016-04-02 15:57:21 +0200

    Valgrind Testaufruf in Shell Script implementiert.

--- printCompiler.sh
+++ printCompiler.sh
@@ -2,4 +2,5 @@
 
 make;
 ./Compiler test.dat out.txt > compiler.txt;
+#valgrind --tool=memcheck --leak-check=yes --show-reachable=yes --num-callers=20 --track-fds=yes ./Compiler test.dat out.txt > compiler.txt;
 gedit compiler.txt;
