5ea93facc364ab22364a2a7b72890e6533d4b2a0
manuel <info@jadecoma.de>
2016-10-07 18:52:44 +0200

    initaler commit der Tree Dateien

---
+++ Tree/src/Tree.cpp
@@ -0,0 +1,18 @@
+/*
+ * Tree.cpp
+ *
+ *  Created on: 07.10.2016
+ *      Author: compiler
+ */
+
+#include "Tree.h"
+
+Tree::Tree() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Tree::~Tree() {
+	// TODO Auto-generated destructor stub
+}
+
