f51e92824b669dc855a3f726e73406b0f6486591
manuel <info@jadecoma.de>
2016-10-08 04:22:41 +0200

    Konstante angepasst

--- Parser/src/Node/Node.cpp
+++ Parser/src/Node/Node.cpp
@@ -49,12 +49,13 @@ void Node::print()
 	
 	if (childrenCount == 0) {
 		if (myInfo->getToken() != NULL) {
-			if (myInfo->getToken()->getTokenType() == TOKEN_IDENTIFIER) {
+			if (myInfo->getToken()->getTokenType() == Token::TT_IDENTIFIER) {
 				// Identifier
 				std::cout << myInfo->getToken()->getLexem();
 			} else {
 				// Keyword
-				std::cout << Const::tokenTypeAsString(myInfo->getToken()->getTokenType());
+				//TODO: reaktivieren
+//				std::cout << Const::tokenTypeAsString(myInfo->getToken()->getTokenType());
 			}
 		}
 	}
