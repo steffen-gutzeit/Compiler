c278c8fa60381f879bbb356147b69ff9aff79ca8
manuel <info@jadecoma.de>
2016-10-08 03:56:37 +0200

    printCompiler.sh entfernt

--- printCompiler.sh
+++
@@ -1,7 +0,0 @@
-
-#!/bin/bash
-
-make;
-./Compiler test.dat out.txt;
-#valgrind --tool=memcheck --leak-check=yes --show-reachable=yes --num-callers=20 --track-fds=yes ./Compiler test.dat out.txt;
-#gedit out.txt;
